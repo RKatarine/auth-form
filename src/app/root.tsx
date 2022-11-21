584d458400f9fce622a421283382c12df0aef97d
rkate <rkatarine@mail.ru>
2022-11-21 21:15:09 +0300

    EM-0 initial commit

---
+++ src/app/root.tsx
@@ -0,0 +1,5 @@
+export const Root = () => (
+    <div>
+        root application
+    </div>
+)
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Root} from '~/app/root';
+
+const rootEl = document.getElementById('root')!;
+ReactDOM.render(<Root />, rootEl);
